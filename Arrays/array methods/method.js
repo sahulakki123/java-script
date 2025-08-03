825e07554bde12e1979104fa1e20ad6b38b92c0a
sahulakki123 <lakkisahus04@gmail.com>
2025-08-03 21:53:32 +0530

    arrays methods

---
+++ Arrays/array methods/method.js
@@ -0,0 +1,41 @@
+//  Arrays in method
+
+
+
+// push():add to end 
+//  push methoed ak eliment to piche se add karta hai
+
+// let items = ["potato", "tomato","apple","litchi"]
+
+// items.push("chips","takata")
+
+// console.log(items);
+
+
+
+// pop(): delete from and & return
+// pop piche se ak eliment ko hatata hai delete item ko dikhata bhi hai
+// let items = ["potato", "tomato","apple","litchi"]
+// console.log(items)
+
+// let del = items.pop()
+// console.log(items)
+
+// console.log(`delete  ${del}`);
+
+
+
+
+// to string():converts array to string
+// 
+
+let items = ["potato", "tomato","apple","litchi"]
+let marks = [55,66,88,90,65,88]
+
+console.log(items)
+console.log(items.toString());
+
+console.log(marks.toString());
+console.log(items);
+
+
